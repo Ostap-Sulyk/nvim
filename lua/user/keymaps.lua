240baa21fa5faddee6c5b0a848c84ef5c9b03f1e
ostap-sulyk <ostapsulyk@gmail.com>
2022-04-26 09:13:43 -0400

    fixed neovim for rust but break for other languages

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -90,4 +90,24 @@ keymap("n", "<Leader>p", ":MarkdownPreview<CR>", opts)
 -- symbol_outline
 keymap("n", "<Leader>o", ":SymbolsOutline<CR>", opts)
 
+-- RUST
+keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+keymap("n", "<Leader>h", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+keymap("n", "<Leader>sh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+keymap("n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+keymap(
+  "n",
+  "gl",
+  '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>',
+  opts
+)
+keymap("n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 
