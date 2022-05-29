e109384fcdcf2d939b7bb46a90da823c4b80e543
ostap_sulyk <ostapsulyk@gmail.com>
2022-05-29 16:00:39 -0400

    m

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,4 @@
-local colorscheme = "onedark"
+local colorscheme = "gruvbox"
 
 
 vim.g.nord_contrast = false
@@ -6,6 +6,17 @@ vim.g.nord_borders = true
 vim.g.nord_italics = false
 vim.g.vscode_style = "dark"
 --vim.g.nord_disable_background = true
+
+vim.g.vscode_style = "dark"
+vim.g.gruvbox_contrast_dark = "medium"
+vim.g.gruvbox_sign_column = "bg0"
+vim.g.gruvbox_italic = 0
+vim.g.gruvbox_bold = 0
+vim.g.gruvbox_color_column = "bg0"
+vim.g.gruvbox_invert_selection = 0
+vim.g.gruvbox_improved_warnings = 1
+vim.g.gruvbox_transparent_bg=true
+
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
